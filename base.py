c8eabdc3af1349c56c367ba4962bae050265475d
kerwei <kerwei84@gmail.com>
2019-06-06 23:44:59 +0800

    Convert dataframe time and demand scalar values into bucketed labels for use with HMM

--- base.py
+++ base.py
@@ -1,10 +1,14 @@
+from collections import Counter
 from datetime import datetime, timedelta
+import geohash2
+from itertools import chain
+import matplotlib.pyplot as plt
+import numpy as np
 import os
 import pandas as pd
 from pandas_schema import Column, Schema
 from pandas_schema.validation import InRangeValidation, MatchesPatternValidation, IsDtypeValidation, InListValidation
-import numpy as np
-import sys
+import pomegranate
 import warnings
 
 import pdb
@@ -57,9 +61,31 @@ An arbritary calendar day 1-1-1900 will be selected as the base date. This can b
 BASEDATE = datetime(1900, 1, 1)
 
 def hrmin_scalar2delta(strseries):
+    """
+    Convert a time string with format hh:mm into its corresponding timedelta unit
+    Base reference time is 00:00
+    """
     for ts in strseries:
         x, y = ts.split(':')
         yield timedelta(hours=int(x), minutes=int(y))
+
+
+def pair_counts(sequences_A, sequences_B):
+    """Return a dictionary keyed to each unique value in the first sequence list
+    that counts the number of occurrences of the corresponding value from the
+    second sequences list.
+    
+    For example, if sequences_A is tags and sequences_B is the corresponding
+    words, then if 1244 sequences contain the word "time" tagged as a NOUN, then
+    you should return a dictionary such that pair_counts[NOUN][time] == 1244
+    """
+    tagset = set(k for i in sequences_A for k in i)
+    res_dct = {i:{} for i in tagset}
+    cntr = Counter(zip(chain(*sequences_A), chain(*sequences_B)))
+    for tag, word in cntr:
+        res_dct[tag][word] = cntr[(tag, word)]    
+    return res_dct
+
 
 # Timedelta at hour and minute precision
 hrmin_delta = []
@@ -71,3 +97,76 @@ time_delta = [x + y for x, y in zip(day_delta, hrmin_delta)]
 # Create the datetime column
 df['reftime'] = [BASEDATE + x for x in time_delta]
 
+
+"""
+--- LATLONG ---
+Only using this for research. In reality, code can just work on the geohash values directly without converting them to latlong
+"""
+# uniqgeo = set(df.geohash6.unique())
+# geotbl = {x:geohash2.decode_exactly(x) for x in uniqgeo}
+# latlong = df.geohash6.map(geotbl)
+# latval = []
+# lngval = []
+
+# for lat, lng, errlat, errlng in latlong:
+#     latval.append(lat)
+#     lngval.append(lng)
+
+# df['lat'] = latval
+# df['lng'] = lngval
+
+
+"""
+--- WEEKENDS AND PH ---
+
+Hypothesis: Weekends and PH have significantly different demand from a regular weekday. These should be isolated
+and analyzed as a separate time series
+"""
+# Try data aggregation at geohash4 to determine weekends. qp03 is used here
+# qp03 = df.loc[df.geohash6 == 'qp09d3']
+# qp03.set_index('reftime', inplace=True)
+# qp03_hour = qp03.groupby([qp03.index.dayofyear, qp03.index.hour])['demand'].mean()
+# qp03_plot = qp03_hour.unstack(level=0)
+
+# # Line plot
+# fig, ax = plt.subplots()
+# for idx in qp03_plot.columns[:15]:
+#     ax.plot(qp03_plot[idx])
+
+# plt.show()
+
+
+"""
+--- MODEL ---
+"""
+
+"""
+qp03mf
+"""
+# Cut the dataset up into training set and testing set
+qp03mf = df.loc[df.geohash6 == 'qp03mf']
+qp03mf.set_index(qp03mf.reftime, inplace=True)
+qp03mf_quart = qp03mf.resample('15T').mean()
+# Fill np.nan demands with 0s
+qp03mf_quart.demand.fillna(0, inplace=True)
+# Patch np.nan days
+qp03mf_quart.day = qp03mf_quart.index.dayofyear
+
+# Apply the labels - resolution of 0.1 normalized demand
+# Resolution at 0.1 normalized demand
+qp03mf_quart['dmd_label'] = [''.join(['D', str(x//0.1)]) for x in qp03mf_quart.demand]
+# Resolution at 0.5 normalized demand
+qp03mf_quart['dmd_label'] = ['H' if x >= qp03mf_quart.demand.median() else 'L' for x in qp03mf_quart.demand]
+# Let's allow 15-min demand forecast to be made as long as we know the demand pattern for the past hour
+# Therefore, tags should be the concatenation of demand for [T-4, T-3, T-2, T-1, T] -> 100,000 possibilities
+# Should I reduce the resolution of these buckets??
+qp03mf_quart['timex'] = qp03mf_quart.index.time
+qp03mf_quart['time_rank'] = qp03mf_quart.timex.rank(method='dense')
+qp03mf_quart['label'] = [''.join(['T', str(x), y]) for x, y in zip(qp03mf_quart.time_rank, qp03mf_quart.dmd_label)]
+qp03mf_quart.drop(['dmd_label', 'timex', 'time_rank'], axis=1, inplace=True)
+qp03mf_quart['rdemand'] = [str(round(x, 2)) for x in qp03mf_quart.demand]
+
+# Split into training and testing sets
+df_train = qp03mf_quart.loc[qp03mf_quart.day <= 48, ['label', 'rdemand']]
+df_test = qp03mf_quart.loc[qp03mf_quart.day > 48, ['label', 'rdemand']]
+pdb.set_trace()
