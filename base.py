f5218c2510cd9339f01d072d37cdab8530226e5b
kerwei <kerwei84@gmail.com>
2019-06-17 08:25:11 +0800

    Convert frame operations into functions. Actual analytics only run from the main block

--- base.py
+++ base.py
@@ -16,38 +16,41 @@ ROOTDIR = os.getcwd()
 DATADIR = os.path.join(ROOTDIR, 'data')
 filename = 'training.csv'
 
-df = pd.read_csv(os.path.join(DATADIR, filename))
+def load_trainset(filename='training.csv'):
+    df = pd.read_csv(os.path.join(DATADIR, filename))
+
+    return df
 
 """
 --- VALIDATION ---
 
 Check that data input is consistent with downstream dataframe treatments. Reason: GIGO
 """
-schema = Schema([
-    Column('geohash6', [MatchesPatternValidation(r'[a-z0-9]{6}')]),
-    Column('day', [IsDtypeValidation(np.int64), InRangeValidation(min=1)]),
-    Column('timestamp', [InListValidation(['20:0', '14:30', '6:15', '5:0', '4:0', '12:15', '3:30', '20:45',
-       '22:15', '9:15', '11:45', '14:45', '2:30', '23:45', '11:30',
-       '10:0', '11:0', '18:30', '6:0', '13:0', '4:30', '15:30', '4:15',
-       '9:0', '0:15', '21:15', '4:45', '12:30', '12:0', '14:15', '9:30',
-       '5:15', '3:15', '16:30', '8:0', '11:15', '18:45', '16:0', '2:15',
-       '7:0', '18:0', '3:0', '15:0', '22:45', '20:30', '0:30', '13:30',
-       '22:0', '5:30', '9:45', '10:30', '17:0', '5:45', '6:30', '23:30',
-       '1:15', '0:45', '1:30', '13:45', '12:45', '2:45', '19:15', '14:0',
-       '13:15', '15:45', '8:45', '23:15', '16:15', '19:30', '21:30',
-       '10:45', '7:15', '7:30', '16:45', '17:15', '23:0', '6:45', '18:15',
-       '1:0', '8:15', '17:45', '22:30', '2:0', '1:45', '7:45', '10:15',
-       '3:45', '8:30', '15:15', '21:0', '21:45', '19:45', '19:0', '0:0',
-       '17:30', '20:15'])]),
-    Column('demand', [InRangeValidation(0, np.nextafter(1, 2))])
-])
+def validate_frame(df):
+    schema = Schema([
+        Column('geohash6', [MatchesPatternValidation(r'[a-z0-9]{6}')]),
+        Column('day', [IsDtypeValidation(np.int64), InRangeValidation(min=1)]),
+        Column('timestamp', [InListValidation(['20:0', '14:30', '6:15', '5:0', '4:0', '12:15', '3:30', '20:45',
+        '22:15', '9:15', '11:45', '14:45', '2:30', '23:45', '11:30',
+        '10:0', '11:0', '18:30', '6:0', '13:0', '4:30', '15:30', '4:15',
+        '9:0', '0:15', '21:15', '4:45', '12:30', '12:0', '14:15', '9:30',
+        '5:15', '3:15', '16:30', '8:0', '11:15', '18:45', '16:0', '2:15',
+        '7:0', '18:0', '3:0', '15:0', '22:45', '20:30', '0:30', '13:30',
+        '22:0', '5:30', '9:45', '10:30', '17:0', '5:45', '6:30', '23:30',
+        '1:15', '0:45', '1:30', '13:45', '12:45', '2:45', '19:15', '14:0',
+        '13:15', '15:45', '8:45', '23:15', '16:15', '19:30', '21:30',
+        '10:45', '7:15', '7:30', '16:45', '17:15', '23:0', '6:45', '18:15',
+        '1:0', '8:15', '17:45', '22:30', '2:0', '1:45', '7:45', '10:15',
+        '3:45', '8:30', '15:15', '21:0', '21:45', '19:45', '19:0', '0:0',
+        '17:30', '20:15'])]),
+        Column('demand', [InRangeValidation(0, np.nextafter(1, 2))])
+    ])
 
-# pandas_schema does not currently support error summary. For validation of big datasets, this results
-# in a big list of errors, which is not useful when printed out to the console. For now, let's just raise an warning and continue
-errors = schema.validate(df)
-if errors:
-    warnings.warn("Data loaded with some inconsistencies. Resulting dataframe may not be accurate.")
+    # pandas_schema does not currently support error summary. For validation of big datasets, this results
+    # in a big list of errors, which is not useful when printed out to the console. For now, let's just raise an warning and continue
+    errors = schema.validate(df)
 
+    return errors
 
 """
 --- DATETIME ---
@@ -78,24 +81,28 @@ def framebygeohash(df, geohashlist):
     return df_cut
 
 
-# All geohashes
-geohash = [x for x in df.geohash6]
+def standard_frame(frame):
+    """
+    Base operations
+    """
+    # Timedelta at hour and minute precision
+    hrmin_delta = []
+    hrmin_delta.extend(hrmin_scalar2delta(frame.timestamp))
+    # Timedelta at day precision. -1 for 1900-01-01
+    day_delta = [timedelta(days=x-1) for x in frame.day]
+    # Timedelta combined
+    time_delta = [x + y for x, y in zip(day_delta, hrmin_delta)]
+    # Create the datetime column
+    frame['reftime'] = [BASEDATE + x for x in time_delta]
+    frame.set_index('reftime', inplace=True)
 
-geofilter = geohash[:10]
-df = framebygeohash(df, geofilter)
+    return frame
 
-# Timedelta at hour and minute precision
-hrmin_delta = []
-hrmin_delta.extend(hrmin_scalar2delta(df.timestamp))
-# Timedelta at day precision. -1 for 1900-01-01
-day_delta = [timedelta(days=x-1) for x in df.day]
-# Timedelta combined
-time_delta = [x + y for x, y in zip(day_delta, hrmin_delta)]
-# Create the datetime column
-df['reftime'] = [BASEDATE + x for x in time_delta]
-df.set_index('reftime', inplace=True)
 
-def standardize_frame(frame):
+def derived_frame(frame):
+    """
+    Create derived columns
+    """
     frame = frame.resample('15T').mean()
     frame.demand.fillna(0, inplace=True)
     frame['day'] = frame.index.dayofyear
@@ -112,13 +119,28 @@ def standardize_frame(frame):
 
 
 if __name__ == '__main__':
+    df = load_trainset()
+    errors = validate_frame(df)
+
+    if errors:
+        warnings.warn("Data loaded with some inconsistencies. Resulting dataframe may not be accurate.")
+
+    # All geohashes
+    geohash = [x for x in df.geohash6]
+    # Filter for desired geohashes
+    geofilter = geohash[:10]
+    df = framebygeohash(df, geofilter)
+
+    # Convert scalar day-time values to datetime objects
+    df = standard_frame(df)
+
     # Split into training and testing sets
     for geo in geofilter:
         df_geo = df.loc[df.geohash6 == geo]
-        df_geo = standardize_frame(df_geo)
+        df_geo = derived_frame(df_geo)
         demand_label = list(df_geo.label.unique())
         test_split = (df_geo.day.max() // 5) * 4
-        pdb.set_trace()
+
         df_train = df_geo.loc[df_geo.day <= test_split, ['label', 'rdemand', 'forward_label']]
         df_test = df_geo.loc[df_geo.day > test_split, ['label', 'rdemand', 'forward_label']]
 
